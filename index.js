735930fcdce9ad7bd07b5a4d0868d92534f06cf3
David Ongaro <davidongaroh@gmil.com>
2022-06-06 22:30:55 +0300

    commit tree bb44adbdecc69c5772ac10fbb6772029805881c7

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+
+  if (ride<= 400) {
+    return 'This one is on me!'
+  }
+  else if (2000<ride && ride<2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else  {
+    return 'No can do.'
+  }
+  scuberGreetingForFeet(199)
+  scuberGreetingForFeet(2001)
+  scuberGreetingForFeet(2501)
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC'? "Ok, sounds good." : 'No go.'
 }
-
-function switchOnCharmFromTip(){
+const tip= "Thank you so much"
+function switchOnCharmFromTip(tip){
   // Write your code here!
+ switch (tip) {
+   case 'generous':
+    return "Thank you so much.";
+  case 'not as generous':
+    return 'Thank you.';
+    break;
+  default:
+    return "Bye."
+}
 }
